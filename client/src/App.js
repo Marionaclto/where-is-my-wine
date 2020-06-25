ef556792ee0828c333e73309d32e044183be37f1
Marta Giménez Galacho <Marta@Andrews-MacBook-Air.local>
2020-06-25 07:07:35 +0200

    head removed and added to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,33 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import './App.css';
 import Home from './components/homePage';
-import RestaurantResults from './components/restaurantResults'
+import RestaurantResults from './components/restaurantResults';
 
 class App extends React.Component {
-  state = {
-    path : '/'
-  }
+	state = {
+		path: '/',
+	};
 
-  handleOnClick = path => {
+	handleOnClick = (path) => {
 		this.setState({ path });
 	};
 
-  render() {
-
-    return(
-      <div className="App">
-        <BrowserRouter>
-          <NavLink to='/'>Home</NavLink>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/search/:wineId' component={RestaurantResults}/>
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App'>
+				<BrowserRouter>
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route path='/search/:wineId' component={RestaurantResults} />
+					</Switch>
+					<NavLink to='/'>
+						<button className='home-button'>HOME</button>
+					</NavLink>
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
 
 export default App;
